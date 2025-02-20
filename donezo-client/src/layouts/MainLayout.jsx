68e5cb8fb7c61bbdbe03c2a0804c0acd286df1d3
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-20 23:58:53 +0600

    added all pages

---
+++ donezo-client/src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../shared/Navbar';
+import Home from '../Pages/Home/Home';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Home />
+        </div>
+    );
+};
+
+export default MainLayout;
--- donezo-client/src/routs/router.jsx
+++ donezo-client/src/routs/router.jsx
@@ -2,10 +2,11 @@ import {
     createBrowserRouter,
    
   } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div className="text-4xl">Hello world!</div>,
+      element: <MainLayout/>,
     },
   ]);
   
