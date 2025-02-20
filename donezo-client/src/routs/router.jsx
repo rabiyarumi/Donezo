226ed60319b3732df3df75b2026af8ee555b4cba
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-20 23:50:22 +0600

    added initial setup

---
+++ donezo-client/src/routs/router.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+   
+  } from "react-router-dom";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-4xl">Hello world!</div>,
+    },
+  ]);
+  
+
+export default router;
