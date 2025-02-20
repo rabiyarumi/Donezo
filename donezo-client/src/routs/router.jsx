57a8efbf717b552fb39fb3bbde6ef5a389668975
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-21 03:16:01 +0600

    setup server

--- donezo-client/src/routs/router.jsx
+++ donezo-client/src/routs/router.jsx
@@ -4,10 +4,13 @@ import {
   } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Login from "../Pages/Login/Login";
+import PrivateRoute from "./PrivateRoute";
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <MainLayout/>,
+      element: <PrivateRoute>
+        <MainLayout/>
+      </PrivateRoute>,
     },
     {
       path: "/login",
--- donezo-client/src/shared/Navbar.jsx
+++ donezo-client/src/shared/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import useAuth from '../hooks/useAuth';
 
 const Navbar = () => {
+    const {user} = useAuth()
     return (
-        <nav className='bg-black py-4 text-center'>
+        <nav className='bg-black py-4 text-center flex justify-between px-10'>
             <p className='text-white text-4xl font-bold'>Donezo</p>
-
+            {
+                user && <p>
+                    <img src={user?.photoURL} alt="" className='h-12 rounded-full' />
+                </p>
+            }
         </nav>
     );
 };
